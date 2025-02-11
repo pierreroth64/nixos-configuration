1514c5abb039bb9bf8f14aaa4d47bedc46b89eac
peio <pierreroth64@gmail.com>
2025-02-11 22:16:53 +0100

    refactor: modules are system wide

--- hosts/adara.nix
+++ hosts/adara.nix
@@ -5,6 +5,15 @@
     ../users/peio.nix
   ];
 
+  # my toggles
+  my.browsers.brave.enable = true;
+  my.browsers.chromium.enable = true;
+  my.browsers.firefox.enable = true;
+  my.editors.vscode.enable = true;
+  my.office.libreoffice.enable = true;
+  my.chat.slack.enable = true;
+  my.chat.whatsapp.enable = true;
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
--- users/peio.nix
+++ users/peio.nix
@@ -6,15 +6,6 @@
         extraGroups = [ "networkmanager" "wheel" ];
     };
 
-    # my toggles
-    my.browsers.brave.enable = true;
-    my.browsers.chromium.enable = true;
-    my.browsers.firefox.enable = true;
-    my.editors.vscode.enable = true;
-    my.office.libreoffice.enable = true;
-    my.chat.slack.enable = true;
-    my.chat.whatsapp.enable = true;
-
     home-manager = {
         extraSpecialArgs = { inherit inputs; };
         users.peio = {
