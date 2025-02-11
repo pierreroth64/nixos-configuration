1f3d7ce9f93267335f7ac340960e9f8855be1e86
peio <pierreroth64@gmail.com>
2025-02-11 10:29:01 +0100

    feat: tweaking home-manager config

--- flake.nix
+++ flake.nix
@@ -4,7 +4,6 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     unstable-pkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -20,6 +19,12 @@
         ./hosts/adara.nix
         ./configuration.nix
         inputs.home-manager.nixosModules.default
+        {
+          home-manager = {
+            useUserPackages = true;
+            useGlobalPkgs = true;
+          };
+        }
       ];
     };
   };
