6470a3787ada6f0ed54717431463003ba90d552d
peio <pierreroth64@gmail.com>
2025-02-14 23:12:55 +0100

    feat(stylix): better cursor

--- modules/system/stylix.nix
+++ modules/system/stylix.nix
@@ -10,5 +10,10 @@
     enable = true;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
     image = ../../assets/sanchese.jpg;
+    cursor = {
+        package = pkgs.bibata-cursors;
+        name = "Bibata-Modern-Ice";
+        size = 24;
+    };
   };
 }
