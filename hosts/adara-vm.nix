c0e8bb0e4e0efad9d0955628726662eadd7536f2
peio <pierreroth64@gmail.com>
2025-04-02 14:08:38 +0200

    feat: improve vm configuration (qemu)

--- hosts/adara-vm.nix
+++ hosts/adara-vm.nix
@@ -24,6 +24,16 @@
         x = 1280;
         y = 800;
       };
+      qemu.options = [
+        # Better display option
+        "-vga virtio"
+        "-display gtk,zoom-to-fit=false"
+        # Enable copy/paste
+        # https://www.kraxel.org/blog/2021/05/qemu-cut-paste/
+        "-chardev qemu-vdagent,id=ch1,name=vdagent,clipboard=on"
+        "-device virtio-serial-pci"
+        "-device virtserialport,chardev=ch1,id=ch1,name=com.redhat.spice.0"
+      ];
     };
   };
 }
