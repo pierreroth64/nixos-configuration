8b55dd72ebd7d03b9099d56f7a04bde4850f6570
peio <pierreroth64@gmail.com>
2025-04-02 14:05:27 +0200

    fix: build vm

--- hosts/adara-vm.nix
+++ hosts/adara-vm.nix
@@ -7,21 +7,23 @@
 
 {
   imports = [
-    <nixpkgs/nixos/modules/virtualisation/qemu-vm.nix>
     ../users/vm-user.nix
   ];
 
   networking = {
-    hostName = "adara-vm";
+    hostName = "adara";
     networkmanager.enable = true;
   };
 
-  virtualisation = {
-    memorySize = 8192;
-    cores = 6;
-    resolution = {
-      x = 1280;
-      y = 800;
+  virtualisation.vmVariant = {
+    # following configuration is added only when building VM with build-vm
+    virtualisation = {
+      memorySize = 8192;
+      cores = 6;
+      resolution = {
+        x = 1280;
+        y = 800;
+      };
     };
   };
 }
