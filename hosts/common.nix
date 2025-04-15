80a2d8845effe44be82051dd146ce5e5e33bc0f4
peio <pierreroth64@gmail.com>
2025-04-15 09:13:43 +0200

    fix: trying X11 to avoid mouse cursor strange behaviour

--- hosts/common.nix
+++ hosts/common.nix
@@ -36,6 +36,7 @@
 
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.wayland = false;
   services.xserver.desktopManager.gnome.enable = true;
 
   services.xserver.xkb = {
