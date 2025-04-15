8df2301a2ec04c695698decb4cc941ba661708fc
peio <pierreroth64@gmail.com>
2025-04-15 09:21:05 +0200

    Revert "fix: trying X11 to avoid mouse cursor strange behaviour"
    This reverts commit 80a2d8845effe44be82051dd146ce5e5e33bc0f4.

--- hosts/common.nix
+++ hosts/common.nix
@@ -36,7 +36,6 @@
 
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.displayManager.gdm.wayland = false;
   services.xserver.desktopManager.gnome.enable = true;
 
   services.xserver.xkb = {
