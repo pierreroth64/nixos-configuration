b481110ae51c0030666c67bdeced1c79f033f0e7
peio <pierreroth64@gmail.com>
2025-12-04 09:27:32 +0100

    feat: nix flake update

--- modules/home/cli/git.nix
+++ modules/home/cli/git.nix
@@ -21,26 +21,28 @@ in
   config = lib.mkIf cfg.git.enable {
     home-manager.users.${userName}.programs.git = {
       enable = true;
-      inherit userName;
-      inherit userEmail;
-      signing = {
-        signByDefault = true;
-        key = userGPGPublicKey;
-      };
-      aliases = {
-        co = "checkout";
-        ci = "commit";
-        br = "branch";
-        ds = "diff --staged";
-        st = "status -sb";
-        unstage = "reset HEAD --";
-        untrack = "rm --cached";
-        ls = "log --pretty=format:'%C(yellow)%h %C(blue)%ad %C(red)%d %C(reset)%s %C(green)[%an]' --decorate --date=short";
-      };
-      extraConfig = {
+      settings = {
+        user = {
+          email = userEmail;
+          name = userName;
+        };
+        alias = {
+          co = "checkout";
+          ci = "commit";
+          br = "branch";
+          ds = "diff --staged";
+          st = "status -sb";
+          unstage = "reset HEAD --";
+          untrack = "rm --cached";
+          ls = "log --pretty=format:'%C(yellow)%h %C(blue)%ad %C(red)%d %C(reset)%s %C(green)[%an]' --decorate --date=short";
+        };
         pull = {
           rebase = "merges";
         };
+      };
+      signing = {
+        signByDefault = true;
+        key = userGPGPublicKey;
       };
     };
   };
